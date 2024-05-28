36076055aa15757f31c3a6b7ad34a2b9ac8769ba
La Quoc Anh <hexagon.study@gmail.com>
2024-05-28 11:51:41 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="w-full h-screen bg-slate-900 flex justify-center items-center">
+      <h1 className="text-white font-black text-7xl">quocanhbk17</h1>
+    </main>
+  );
+}
+
+export default App;
